8fdf5da1c1209a0c62171ac13782b0bd1df34359
josevictorsv <joseeviictor@gmail.com>
2022-09-23 00:32:18 -0300

    Atualizando fundamentos

--- fundamentos/numerosCuidados.js
+++ fundamentos/numerosCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0) //infinito
 console.log("10" / 2) //js consegue dividir
 console.log("Show!" * 2)
-console.log(0.1 + 0.7)
+console.log(0.1 + 0.7) 
+// console.log(10.toString)
+console.log((10.34).toFixed(2))
 
--- fundamentos/tipagemFraca.js
+++ fundamentos/tipagemFraca.js
@@ -6,3 +6,7 @@ qualquer = 3.1516
 console.log(qualquer)
 console.log(typeof qualquer)
 
+//Evitar nomes genéricos e siglas
+
+let valor = ''
+let numero = 1
